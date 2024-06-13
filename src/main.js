6a4aa07d3bdb91f5bb073a3004ce7a7e5c654078
vincent mendoza <99225630+Mendezvincent@users.noreply.github.com>
2024-06-13 14:39:21 +0000

    terminado

--- src/main.js
+++ src/main.js
@@ -1,36 +1,48 @@
     //numero introduciod por el usuario
-    const numeroUser = document.getElementById('inputN');
+    const inputNum = document.getElementById('numero');
     //numero mostrado
-    const numeroM = document.querySelector('#winNumeroP');
-    //pantalla de ganar o perder
-    const G_P = document.querySelector('#win');
+    const numeroM = document.querySelector('.cout');
+    //pantalla de ganar o perder proxi
+    const win_screen = document.querySelector('.win');
+    const por_poco_screem =  document.querySelector('por-poco');
+    const lose_screen = document.querySelector('.lose');
     //boton
-    const btn = document.getElementById('btn');
+    const btn_adivinar = document.getElementById('btn_adivinar');
     //numero random
-    let numero = Math.floor(Math.random() * 100);
+    let random = Math.floor(Math.random() * 30);
     
 
-    btn.addEventListener('click', () => {
-        if(parseInt(numero) == parseInt(numeroUser.value)){
-            G_P.textContent = "GANASTE :)";
-            numeroM.textContent = numero;
-            document.getElementById('inputN').value = " ";
-            document.querySelector('#winNumeroP').value = numeroUser;
-            setTimeout(function(){
-                    G_P.textContent = " ";
-                    numeroM.textContent = " ";
-                    document.getElementById('inputN').focus();
-            } ,2000);
-        }else{
-        G_P.textContent = "PERDISTE :(";
-        numeroM.textContent = parseInt(numeroUser.value);
-            document.getElementById('inputN').value = " ";
-            document.querySelector('#winNumeroP').value = numeroUser;
-            setTimeout(function(){
-                    G_P.textContent = " ";
-                    numeroM.textContent = " ";
-                    document.getElementById('inputN').focus();
-            } ,2000);
-        }
-        
+    btn_adivinar.addEventListener('click', () => {
+        //VALIDAR SI  LO  DATOS SOLO SON  NUMEROS
+        inputNum.addEventListener("input", function() {
+            this.value = this.value.replace(/[^0-9]/g, "");
+            if(random === numeroUser.value){
+                inputNum.value = "";
+                win_screen.classList.remove("off");
+                win_screen.classList.add("por-poco");
+                setTimeout(function(){
+                    por_poco_screem.classList.remove("por-poco");
+                    por_poco_screem.classList.add("off");
+                } ,2000);
+            }
+            if(random < numeroUser.value){
+                inputNum.value = "";
+                win_screen.classList.remove("off");
+                win_screen.classList.add("win");
+                setTimeout(function(){
+                    win_screen.classList.remove("win");
+                    win_screen.classList.add("off");
+                } ,2000);
+            }
+            
+            else{
+                lose_screen.classList.remove("off");
+                lose_screen.classList.add("lose");
+                setTimeout(function(){
+                    lose_screen.classList.remove("lose");
+                    lose_screen.classList.add("off");
+                        document.getElementById('inputN').focus();
+                } ,2000);
+            }
+        });        
     });
