c9a8dd2b6c213f1826d17a14eb78230761c1e22c
vincent mendoza <99225630+Mendezvincent@users.noreply.github.com>
2024-06-13 17:46:59 +0000

    XD

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@
     //numero mostrado
     const numeroM = document.querySelector('.cout');
     //pantalla de ganar o perder proxi
-    const win_screen = document.querySelector('.win');
-    const por_poco_screem =  document.querySelector('por-poco');
-    const lose_screen = document.querySelector('.lose');
+    const win_screen = document.querySelector('.off');
+    const por_poco_screem =  document.querySelector('.off');
+    const lose_screen = document.querySelector('.off');
     //boton
     const btn_adivinar = document.getElementById('btn_adivinar');
     //numero random
@@ -16,33 +16,33 @@
         //VALIDAR SI  LO  DATOS SOLO SON  NUMEROS
         inputNum.addEventListener("input", function() {
             this.value = this.value.replace(/[^0-9]/g, "");
-            if(random === numeroUser.value){
-                inputNum.value = "";
-                win_screen.classList.remove("off");
-                win_screen.classList.add("por-poco");
-                setTimeout(function(){
-                    por_poco_screem.classList.remove("por-poco");
-                    por_poco_screem.classList.add("off");
-                } ,2000);
-            }
-            if(random < numeroUser.value){
-                inputNum.value = "";
-                win_screen.classList.remove("off");
-                win_screen.classList.add("win");
-                setTimeout(function(){
-                    win_screen.classList.remove("win");
-                    win_screen.classList.add("off");
-                } ,2000);
-            }
-            
-            else{
-                lose_screen.classList.remove("off");
-                lose_screen.classList.add("lose");
-                setTimeout(function(){
-                    lose_screen.classList.remove("lose");
-                    lose_screen.classList.add("off");
-                        document.getElementById('inputN').focus();
-                } ,2000);
-            }
         });        
+        if(random == inputNum.value){
+            inputNum.value = "";
+            win_screen.classList.remove("off");
+            win_screen.classList.add("win");
+            setTimeout(function(){;
+                win_screen.classList.remove("win");
+                win_screen.classList.add("off");
+            } ,2000);
+        }
+        if(random >= inputNum.value){
+            inputNum.value = "";
+            win_screen.classList.remove("off");
+            win_screen.classList.add("por-poco");
+            setTimeout(function(){
+                win_screen.classList.remove("por-poco");
+                win_screen.classList.add("off");
+            } ,2000);
+        }
+        else{
+
+            lose_screen.classList.add("lose");
+            numeroM.innerHTML = random;
+            setTimeout(function(){
+                lose_screen.classList.remove("lose");
+                lose_screen.classList.add("off");
+                inputNum.focus();
+            } ,2000);
+        }
     });
